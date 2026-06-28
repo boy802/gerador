1e506934383d405582be492d3bc32f372dd38e25
boy802 <santi801v@gmail.com>
2026-06-28 09:34:13 -0300

    Create historyModel.js

---
+++ models/historyModel.js
@@ -0,0 +1,11 @@
+const db = require('../database/db');
+
+exports.create = ({ code_id, user_id, origin }) => {
+  return db.prepare('INSERT INTO history (code_id, user_id, origin) VALUES (?, ?, ?)').run(code_id, user_id, origin);
+};
+
+exports.getAll = () => db.prepare('SELECT h.*, c.code, u.username FROM history h JOIN codes c ON h.code_id = c.id JOIN users u ON h.user_id = u.id ORDER BY h.created_at DESC').all();
+
+exports.getLastUsed = () => db.prepare('SELECT c.code, h.created_at FROM history h JOIN codes c ON h.code_id = c.id ORDER BY h.created_at DESC LIMIT 5').all();
+
+exports.reset = () => db.prepare('DELETE FROM history').run();
