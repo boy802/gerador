7aa47cb058975f9e154b691e2f18b387afa3806d
boy802 <santi801v@gmail.com>
2026-06-28 09:43:04 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,26 @@ function showToast(message) {
   document.body.appendChild(toast);
   setTimeout(() => toast.remove(), 3000);
 }
+
+function showModal(content) {
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+  modal.innerHTML = `<div class="modal-content">${content}<button onclick="closeModal()">Fechar</button></div>`;
+  document.body.appendChild(modal);
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  document.querySelector('.modal').remove();
+}
+
+function showLoading() {
+  const loader = document.createElement('div');
+  loader.className = 'loading';
+  document.body.appendChild(loader);
+}
+
+function hideLoading() {
+  const loader = document.querySelector('.loading');
+  if (loader) loader.remove();
+}
